56a14bda850a24eff7a59d63a5c0544620cb9434
herio <heriothiago@gmail.com>
2019-06-10 10:17:31 -0300

    config banco

--- src/main/java/br/com/sabrina/sgt/dao/JPAConfig.java
+++ src/main/java/br/com/sabrina/sgt/dao/JPAConfig.java
@@ -8,7 +8,6 @@ import org.apache.commons.dbcp2.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.orm.jpa.JpaTransactionManager;
@@ -21,7 +20,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @EnableTransactionManagement
 @EnableJpaRepositories("br.com.sabrina.sgt.dao")
-@PropertySource("classpath:database.properties")
 public class JPAConfig {
 
 	@Autowired
@@ -47,10 +45,10 @@ public class JPAConfig {
 	@Bean
 	public DataSource getDataSource() {
 		BasicDataSource dataSource = new BasicDataSource();
-		dataSource.setDriverClassName(env.getProperty("database.driverClassName"));
-		dataSource.setUrl(env.getProperty("database.url"));
-		dataSource.setUsername(env.getProperty("database.username"));
-		dataSource.setPassword(env.getProperty("database.password"));
+		dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
+		dataSource.setUrl(env.getProperty("spring.datasource.url"));
+		dataSource.setUsername(env.getProperty("spring.datasource.username"));
+		dataSource.setPassword(env.getProperty("spring.datasource.password"));
 		return dataSource;
 	}
 
